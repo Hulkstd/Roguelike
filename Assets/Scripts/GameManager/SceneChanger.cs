64e58b2697da75676376a0a07d42e3326d65c120
HulkSTD <hulk1257@gmail.com>
2019-04-23 11:42:42 +0900

    a

---
+++ Assets/Scripts/GameManager/SceneChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static SceneChanger Instance { get; private set; }
+
+    #region Variables
+    #endregion
+
+    #region Functions
+
+    #region Public
+    public void SceneChangeWithLoadingScene(string SceneName)
+    {
+        StartCoroutine(ChangeScene(SceneName));
+    }
+
+    public void SceneChange(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    #endregion
+
+    #region Private
+    private IEnumerator ChangeScene(string SceneName)
+    {
+        SceneManager.LoadScene("Loading");
+
+        SceneManager.LoadSceneAsync(SceneName);
+
+        yield return null;
+    }
+    #endregion
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/LoadingText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingText : MonoBehaviour
+{
+    private UnityEngine.UI.Text text;
+
+    void Start()
+    {
+        text = GetComponent<UnityEngine.UI.Text>();
+
+        StartCoroutine(ChangeText());
+    }
+
+    IEnumerator ChangeText()
+    {
+        while(true)
+        {
+            text.text = "Loading .";
+            yield return new WaitForSeconds(1.0f);
+
+            text.text = "Loading . .";
+            yield return new WaitForSeconds(1.0f);
+
+            text.text = "Loading . . .";
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
