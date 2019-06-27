726336c5a96e64ca99d84a405b965b78edfca987
Jinsab <kimjinou116@naver.com>
2019-06-27 17:13:33 +0900

    [Create] TestScene Mouse Effect
    Mouse trail, Image, Particle System, Scripts(ClickChecker, etc)

---
+++ Assets/Scripts/UI/ClickCheaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCheaker : MonoBehaviour
+{
+    //protected bool letPlay = true;
+    public ParticleSystem followingparticle;
+
+    public void Update()
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 newPosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            followingparticle.transform.position = newPosition - Vector3.forward * newPosition.z;
+            followingparticle.Play();
+        }
+        else
+        {
+            followingparticle.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MouseTracer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracer : MonoBehaviour
+{
+    public GameObject followingCube;
+    private float initialZPos = 0f;
+
+    void Awake() 
+    { 
+        initialZPos = followingCube.transform.position.y; 
+    }
+
+    void Update () 
+    {
+        Camera camera = GetComponent<Camera>();
+        Vector3 newPosition = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        //followingCube.transform.position = new Vector3(newPosition.x, followingCube.transform.position.y, 0f);
+        followingCube.transform.position = newPosition - Vector3.forward * newPosition.z;
+
+        //print("X : " + newPosition.x + ", Y : " + newPosition.y); 
+    } 
+}
