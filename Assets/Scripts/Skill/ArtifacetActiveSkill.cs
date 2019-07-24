e2f10bfdd25c7e162f23a9a704881b4e32698007
c0cac01a <joobs1127@gmail.com>
2019-07-24 21:12:33 +0900

    write json

---
+++ Assets/Scripts/Skill/ArtifacetActiveSkill.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ArtifacetActiveSkill
+{
+    public delegate void ActiveSkill();
+
+    private static void FireBall() { }
+}
